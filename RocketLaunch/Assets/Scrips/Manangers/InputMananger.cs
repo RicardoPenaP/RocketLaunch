a4eb7cc04d2957a741e5e5dbf98abd21cba218f2
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-14 14:16:47 -0300

    Implemented gamplay feature
    Implemented Input Mananger input catch from the player

--- RocketLaunch/Assets/Scrips/Manangers/InputMananger.cs
+++ RocketLaunch/Assets/Scrips/Manangers/InputMananger.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputMananger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static InputMananger Instance { get; private set; }
+
+    private PlayerInputActions inputActions;
+
+    private void Awake()
     {
-        
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+
+        inputActions = new PlayerInputActions();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions.Enable();
     }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    public bool TryGetSideMovementInput(out Vector2 sideMovementVector)
+    {
+        if (inputActions.Player.SideMovement.triggered)
+        {
+            sideMovementVector = inputActions.Player.SideMovement.ReadValue<Vector2>();
+            return true;
+        }
+        else
+        {
+            sideMovementVector = Vector2.zero;
+            return false;
+        }
+    }
+
+    public bool GetStartEngineInput()
+    {        
+        return inputActions.Player.StartEnginge.IsInProgress();
+    }
+
+
 }
