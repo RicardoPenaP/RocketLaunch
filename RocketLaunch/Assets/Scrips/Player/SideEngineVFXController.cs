f10bce1f9aa2274b714a1ea1a732054807cd89ca
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-15 19:17:18 -0300

    Renamed class

--- RocketLaunch/Assets/Scrips/Player/SideEngineVFXController.cs
+++ RocketLaunch/Assets/Scrips/Player/SideEngineVFXController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EngineVFXController : MonoBehaviour
+public class SideEngineVFXController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
