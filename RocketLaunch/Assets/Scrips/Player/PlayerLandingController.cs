d34a5b4c25985759cfd3cacadb4b0b59cdc275e5
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-16 15:35:21 -0300

    Implemented feature
    Implemented landing detection

--- RocketLaunch/Assets/Scrips/Player/PlayerLandingController.cs
+++ RocketLaunch/Assets/Scrips/Player/PlayerLandingController.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerLandingController : MonoBehaviour
 {
     [Header("Player Landing Controller")]
+    [Header("Landing Settings")]
     [SerializeField] private float landingMinDistance = 1f;
     [SerializeField] private float prelandingDuration = 1f;
     [SerializeField] private float landingDuration = 5f;
+    [SerializeField] private float rotationSensibility = 1f;
+    [SerializeField] private float rotationForce = 100f;
     [SerializeField] private LayerMask platformsLayerMask;
 
+    public event EventHandler OnLandingStart;
+
     private LevelPlatform landingPlatform;
+    private new Rigidbody rigidbody;
+
+    private bool isLanding = false;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         if (TryGetLandingPlatform())
         {
+            if (InputMananger.Instance.GetInteractInputWasTriggered())
+            {
+                StartLanding();
+            }
+        }
 
+        if (isLanding)
+        {
+            LandingUpdate();
         }
     }
 
@@ -37,5 +59,20 @@ public class PlayerLandingController : MonoBehaviour
 
         landingPlatform = null;
         return false;
+    }
+
+    private void StartLanding()
+    {
+        isLanding = true;
+        OnLandingStart?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void LandingUpdate()
+    {
+        if (InputMananger.Instance.TryGetRotationDirectionInput(out float rotationDirection))
+        {
+            Vector3 torqueAxis = Vector3.forward * rotationForce * rotationDirection * rotationSensibility * Time.deltaTime;
+            rigidbody.AddTorque(torqueAxis, ForceMode.Impulse);
+        }
     }
 }
