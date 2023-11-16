3b58a335800eef4593efcf798a456e58671b9852
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-16 19:20:43 -0300

    Updated camera mananger
    Updated and refactorized the class code

--- RocketLaunch/Assets/Scrips/Cameras/CameraMananger.cs
+++ RocketLaunch/Assets/Scrips/Cameras/CameraMananger.cs
@@ -5,19 +5,24 @@ using System;
 
 public class CameraMananger : MonoBehaviour
 {
+    private enum GameCamera { Still, Moving, Landing}
     [Header("Camera Mananger")]
     [SerializeField] private GameObject stillCinemachineVirtualCamera;
     [SerializeField] private GameObject movingCinemachineVirtualCamera;
-    [SerializeField,Min(0f)] private float changeCameraTime = 3f;
+    [SerializeField] private GameObject landingCinemachineVirtualCamera;
 
-    private GameObject targetCamera;
-    private GameObject currentCamera;
+    [SerializeField,Min(0f)] private float changeToStillCameraTime = 3f;
 
     private PlayerMovement playerMovement;
+    private PlayerLandingController playerLandingController;
+
+    private GameCamera currentCamera;
 
     private void Awake()
     {
         playerMovement = FindObjectOfType<PlayerMovement>();
+        playerLandingController = playerMovement.GetComponent<PlayerLandingController>();
+        currentCamera = GameCamera.Moving;
     }
 
     private void Start()
@@ -26,7 +31,12 @@ public class CameraMananger : MonoBehaviour
         {
             playerMovement.OnStartMovingUpwards += PlayerMovement_OnStartMovingUpwards;
             playerMovement.OnStopMovingUpwards += PlayerMovement_OnStopMovingUpwards;
-        }      
+        }
+
+        if (playerLandingController)
+        {
+            playerLandingController.OnPreLandingStart += PlayerLandingController_OnPreLandingStart;
+        }
     }
 
     private void OnDestroy()
@@ -36,13 +46,52 @@ public class CameraMananger : MonoBehaviour
             playerMovement.OnStartMovingUpwards -= PlayerMovement_OnStartMovingUpwards;
             playerMovement.OnStopMovingUpwards -= PlayerMovement_OnStopMovingUpwards;
         }
+
+        if (playerLandingController)
+        {
+            playerLandingController.OnPreLandingStart -= PlayerLandingController_OnPreLandingStart;
+        }
+    }
+
+    private void SetCurrentGameCamera( GameCamera gameCamera)
+    {
+        switch (currentCamera)
+        {
+            case GameCamera.Still:
+                stillCinemachineVirtualCamera.SetActive(false);
+                break;
+            case GameCamera.Moving:
+                movingCinemachineVirtualCamera.SetActive(false);
+                break;
+            case GameCamera.Landing:
+                landingCinemachineVirtualCamera.SetActive(false);
+                break;
+            default:
+                break;
+        }
+
+        currentCamera = gameCamera;
+
+        switch (currentCamera)
+        {
+            case GameCamera.Still:
+                stillCinemachineVirtualCamera.SetActive(true);
+                break;
+            case GameCamera.Moving:
+                movingCinemachineVirtualCamera.SetActive(true);
+                break;
+            case GameCamera.Landing:
+                landingCinemachineVirtualCamera.SetActive(true);
+                break;
+            default:
+                break;
+        }
     }
 
     private void PlayerMovement_OnStartMovingUpwards(object sender, EventArgs e)
     {
         StopAllCoroutines();
-        stillCinemachineVirtualCamera.SetActive(false);
-        movingCinemachineVirtualCamera.SetActive(true);   
+        SetCurrentGameCamera(GameCamera.Moving);  
     }
 
     private void PlayerMovement_OnStopMovingUpwards(object sender, EventArgs e)
@@ -50,10 +99,14 @@ public class CameraMananger : MonoBehaviour
         StartCoroutine(ChangeCameraRoutine());
     }
 
+    private void PlayerLandingController_OnPreLandingStart(object sender, EventArgs e)
+    {
+        SetCurrentGameCamera(GameCamera.Landing);
+    }
+
     private IEnumerator ChangeCameraRoutine()
     {
-        yield return new WaitForSeconds(changeCameraTime);
-        movingCinemachineVirtualCamera.SetActive(false);
-        stillCinemachineVirtualCamera.SetActive(true);
+        yield return new WaitForSeconds(changeToStillCameraTime);
+        SetCurrentGameCamera(GameCamera.Still);
     }
 }
--- RocketLaunch/Assets/Scrips/Manangers/LevelMananger.cs
+++ RocketLaunch/Assets/Scrips/Manangers/LevelMananger.cs
@@ -60,4 +60,6 @@ public class LevelMananger : MonoBehaviour
         Debug.Log("Level finished");
     }
 
+   
+
 }
