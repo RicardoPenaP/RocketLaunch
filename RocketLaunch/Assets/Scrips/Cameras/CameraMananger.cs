6c210200ccc7cfe839a9ebf31ed76b9d98365ade
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-15 21:18:47 -0300

    Created class

---
+++ RocketLaunch/Assets/Scrips/Cameras/CameraMananger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMananger : MonoBehaviour
+{
+    [Header("Camera Mananger")]
+    [SerializeField] private GameObject stillCinemachineVirtualCamera;
+    [SerializeField] private GameObject movingCinemachineVirtualCamera;
+
+
+}
