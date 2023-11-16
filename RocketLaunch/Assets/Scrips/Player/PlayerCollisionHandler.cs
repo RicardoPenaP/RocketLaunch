d638f1c6b64c13f34b71b12e3040999253acb0dd
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-16 16:32:16 -0300

    Created and implemented class
    Created and implemented Player Collision Handler

--- RocketLaunch/Assets/Scrips/Player/PlayerCollisionHandler.cs
+++ RocketLaunch/Assets/Scrips/Player/PlayerCollisionHandler.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CollisionInfo<T> : EventArgs where T : MonoBehaviour
     {
+        public T collisionObject;
+
+        public CollisionInfo( T collisionObject)
+        {
+            this.collisionObject = collisionObject;
+        }
+    }
+
+    public event EventHandler OnCollisionEnterWithLevelPlatform;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.TryGetComponent<LevelPlatform>(out LevelPlatform levelPlatform))
+        {
+            OnCollisionEnterWithLevelPlatform?.Invoke(this, new CollisionInfo<LevelPlatform>(levelPlatform));
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
