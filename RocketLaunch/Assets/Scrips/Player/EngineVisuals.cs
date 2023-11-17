c2c05265f12cd2924a8acb630903c1e64a545d53
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-17 19:46:29 -0300

    Implemented feature
    Implemented Engine Visual class functionalities

---
+++ RocketLaunch/Assets/Scrips/Player/EngineVisuals.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineVisuals : MonoBehaviour
+{
+    [Header("Engine Visuals")]
+    [SerializeField] UIBar enginePowerBar;
+    [SerializeField] UIBar engineTemperatureBar;
+
+    private EngineController engineController;
+
+    private void Awake()
+    {
+        engineController = FindObjectOfType<EngineController>();
+    }
+
+    private void Start()
+    {
+        if (engineController)
+        {
+            engineController.OnEnginePowerChange += EngineController_OnEnginePowerChange;
+            engineController.OnEngineTemperatureChange += EngineController_OnEngineTemperatureChange;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (engineController)
+        {
+            engineController.OnEnginePowerChange -= EngineController_OnEnginePowerChange;
+            engineController.OnEngineTemperatureChange -= EngineController_OnEngineTemperatureChange;
+        }
+    }
+
+    private void EngineController_OnEnginePowerChange(float currentValue, float maxValue)
+    {
+        enginePowerBar.UpdateFill(currentValue, maxValue);
+    }
+
+    private void EngineController_OnEngineTemperatureChange(float currentValue, float maxValue)
+    {
+        engineTemperatureBar.UpdateFill(currentValue, maxValue);
+    }
+}
