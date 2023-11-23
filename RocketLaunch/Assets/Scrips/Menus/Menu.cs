08265b487f7ab6ae217bd6534d255b829660a00d
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-23 12:58:27 -0300

    Implemented feature
    Implemented Menu abstract class functionalities

--- RocketLaunch/Assets/Scrips/Menus/Menu.cs
+++ RocketLaunch/Assets/Scrips/Menus/Menu.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Menu : MonoBehaviour
+public abstract class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private readonly int OPEN_MENU_ANIMATION_HASH = Animator.StringToHash("OpenMenu");
+    private readonly int CLOSE_MENU_ANIMATION_HASH = Animator.StringToHash("CloseMenu");
+    public event Action OnMenuOpened;
+    public event Action OnMenuClosed;
+
+    private Animator animator;
+
+    protected virtual void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void OpenMenu()
     {
-        
+        OnMenuOpened?.Invoke();
+        animator.SetTrigger(OPEN_MENU_ANIMATION_HASH);
+    
+    }
+
+    protected virtual void CloseMenu()
+    {
+        OnMenuClosed?.Invoke();
+        animator.SetTrigger(CLOSE_MENU_ANIMATION_HASH);
     }
 }
