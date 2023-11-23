04a783249b2b3e396a217ddcefe1c99866ef40be
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-23 13:03:59 -0300

    Reorganized assets

--- RocketLaunch/Assets/Scrips/Menus/Menu.cs
+++ RocketLaunch/Assets/Scrips/Menus/Menu.cs
@@ -7,8 +7,12 @@ public abstract class Menu : MonoBehaviour
 {
     private readonly int OPEN_MENU_ANIMATION_HASH = Animator.StringToHash("OpenMenu");
     private readonly int CLOSE_MENU_ANIMATION_HASH = Animator.StringToHash("CloseMenu");
+
     public event Action OnMenuOpened;
     public event Action OnMenuClosed;
+
+    private Action onOpenAnimationEndedActions;
+    private Action onCloseAnimationEndedActions;
 
     private Animator animator;
 
@@ -17,16 +21,29 @@ public abstract class Menu : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    protected virtual void OpenMenu()
+    protected virtual void OpenMenu(Action onOpenAnimationEndedActions = null)
     {
         OnMenuOpened?.Invoke();
         animator.SetTrigger(OPEN_MENU_ANIMATION_HASH);
-    
+        this.onOpenAnimationEndedActions = onOpenAnimationEndedActions;
     }
 
-    protected virtual void CloseMenu()
+    protected virtual void CloseMenu(Action onCloseAnimationEndedActions = null)
     {
         OnMenuClosed?.Invoke();
         animator.SetTrigger(CLOSE_MENU_ANIMATION_HASH);
+        this.onCloseAnimationEndedActions = onCloseAnimationEndedActions;
     }
+
+    private void Animator_OpenMenuAnimationFinished()
+    {
+        onOpenAnimationEndedActions?.Invoke();
+    }
+
+    private void Animator_CloseMenuAnimationFinished()
+    {
+        onCloseAnimationEndedActions?.Invoke();
+    }
+
+   
 }
