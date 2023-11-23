b6a774dbcc451540c75850db1a33a0383d471099
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-22 23:20:41 -0300

    Updated class
    Updated Pickup class movement and behaviours

--- RocketLaunch/Assets/Scrips/Pickups/Pickup.cs
+++ RocketLaunch/Assets/Scrips/Pickups/Pickup.cs
@@ -5,9 +5,50 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public enum PickupType { Cooland, Fuel}
+
+    private static Transform playerTransform;
     [Header("Pickup")]
     [SerializeField] private PickupType pickupType;
+    [SerializeField] private float pickupRange = 4f;
     [SerializeField,Range(0,100)] private int effectivePercentage;
+    [SerializeField] private float defaultMovementSpeed = 3f;
+    [SerializeField, Range(0f, 1f)] private float movementSpeedAugmentCoeficient = 0.2f;
+
+    private float currentMovementSpeed;
+
+    public bool IsBeenAtractedToThePlayer { get; private set; }
+
+    private void Awake()
+    {
+        currentMovementSpeed = defaultMovementSpeed;
+    }
+
+    private void Start()
+    {
+        if (!playerTransform)
+        {
+            playerTransform = FindObjectOfType<PlayerController>().transform;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerTransform)
+        {
+            MoveTowardsThePlayer();
+        }        
+    }
+
+    private void MoveTowardsThePlayer()
+    {
+        if (Vector3.Distance(transform.position,playerTransform.position) >= pickupRange)
+        {
+            return;
+        }
+        Vector3 movementDirection = (playerTransform.position - transform.position).normalized;
+        transform.position += movementDirection * currentMovementSpeed * Time.deltaTime;
+        currentMovementSpeed += currentMovementSpeed * movementSpeedAugmentCoeficient * Time.deltaTime;
+    }
 
     public PickupType GetPickupType()
     {
@@ -17,5 +58,10 @@ public class Pickup : MonoBehaviour
     public int GetEffectivePercentage()
     {
         return effectivePercentage;
+    }
+    
+    public void PickPickup()
+    {
+        Destroy(gameObject);
     }
 }
--- RocketLaunch/Assets/Scrips/Player/PickupsController.cs
+++ RocketLaunch/Assets/Scrips/Player/PickupsController.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PickupsController : MonoBehaviour
 {
-    
+    [Header("Pickups Controller")]
+    [SerializeField] private LayerMask pickupsLayer;
+    [SerializeField] private float magneticRange = 2.3f;
+
+    [Header("Gizmos Settings")]
+    [SerializeField] private bool showGizmos = false;
+
+    public event Action<Pickup> OnPickupPicked;
+
+    private void OnDrawGizmos()
+    {
+        if (!showGizmos)
+        {
+            return;
+        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, magneticRange);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Pickup>(out Pickup pickup))
+        {
+            OnPickupPicked?.Invoke(pickup);
+            pickup.PickPickup();
+        }
+    }
 }
