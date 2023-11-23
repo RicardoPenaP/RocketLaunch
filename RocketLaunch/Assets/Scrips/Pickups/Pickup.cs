01b3a79de77e4606455b89ad936cad66ce786370
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-22 21:12:13 -0300

    Created class
    Created Pickup class

--- RocketLaunch/Assets/Scrips/Pickups/Pickup.cs
+++ RocketLaunch/Assets/Scrips/Pickups/Pickup.cs
@@ -7,8 +7,15 @@ public class Pickup : MonoBehaviour
     public enum PickupType { Cooland, Fuel}
     [Header("Pickup")]
     [SerializeField] private PickupType pickupType;
-    [SerializeField,Range(0,100)] private float effectivePercentage;
+    [SerializeField,Range(0,100)] private int effectivePercentage;
 
+    public PickupType GetPickupType()
+    {
+        return pickupType;
+    }
 
-
+    public int GetEffectivePercentage()
+    {
+        return effectivePercentage;
+    }
 }
