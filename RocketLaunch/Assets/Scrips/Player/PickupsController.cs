1156ecc188fc17f4ac1524816a72a3f3a8b69278
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-22 23:33:50 -0300

    Implemented feature
    Implemented Pickups Controller class functionalities

--- RocketLaunch/Assets/Scrips/Player/PickupsController.cs
+++ RocketLaunch/Assets/Scrips/Player/PickupsController.cs
@@ -5,5 +5,27 @@ using System;
 
 public class PickupsController : MonoBehaviour
 {
-    
+    private PlayerCollisionHandler playerCollisionHandler;
+    public Action<Pickup> OnPickupPiked;
+
+    private void Awake()
+    {
+        playerCollisionHandler = GetComponentInParent<PlayerCollisionHandler>();
+    }
+
+    private void Start()
+    {
+        if (playerCollisionHandler)
+        {
+            playerCollisionHandler.OnTriggerEnterWithObject += PlayerCollisionHandler_OnTriggerEnterWithObject;
+        }
+    }
+
+    private void PlayerCollisionHandler_OnTriggerEnterWithObject(object sender, EventArgs e)
+    {
+        if (e is PlayerCollisionHandler.CollisionInfo<Pickup> collisionInfo)
+        {
+            OnPickupPiked?.Invoke(collisionInfo.collisionObject);
+        }
+    }
 }
