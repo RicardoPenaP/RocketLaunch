184242b8443a5f9024b95ca7efbbdf3e59811756
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-23 00:41:54 -0300

    Implemented feature
    Implemented Floating Effect class functionalities

--- RocketLaunch/Assets/Scrips/Pickups/FloatingEffect.cs
+++ RocketLaunch/Assets/Scrips/Pickups/FloatingEffect.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class FloatingEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Floating Effect")]
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float maxYOffset;
+
+    private  float maxY, minY, yMovementDirection;    
+
+    private void Start()
     {
-        
+        float startingY = transform.position.y;
+        maxY = startingY + maxYOffset;
+        minY = startingY - maxYOffset;
+        yMovementDirection = -1;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
     }
+
+    private void Move()
+    {
+        Vector3 rotation = Vector3.up * rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotation);
+
+        Vector3 newPosition = transform.position + Vector3.up * yMovementDirection * rotationSpeed * Time.deltaTime;
+        newPosition.y = Mathf.Clamp(newPosition.y,minY,maxY);
+
+        if (newPosition.y >= maxY || newPosition.y <= minY)
+        {
+            yMovementDirection *= -1;
+        }
+
+        transform.position = newPosition;
+    }
+
+    
 }
