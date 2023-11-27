51d2d1015014b499c32c71e5f5f2b6001e88b226
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-27 18:04:11 -0300

    Implemented feature
    Implemented obstacle rotation functionality

--- RocketLaunch/Assets/Scrips/Obstacles/ObstacleController.cs
+++ RocketLaunch/Assets/Scrips/Obstacles/ObstacleController.cs
@@ -28,11 +28,16 @@ public class ObstacleController : MonoBehaviour
     [SerializeField, Min(0f)] private float expansionSpeed = 0f;
     [SerializeField] private Vector3 expansionDirection;
 
+    [Header("Rotation Settings")]
+    [SerializeField] private bool rotationActive = false;
+    [SerializeField] private float rotationSpeed = 20;
+
     public event EventHandler OnTimerStop;
 
     private float timer;
     private Vector3 maxPosition;
-    private Vector3 startingPosition;   
+    private Vector3 startingPosition;
+    private Vector3 rotationDirection;
 
     private void Awake()
     {        
@@ -43,7 +48,7 @@ public class ObstacleController : MonoBehaviour
         timer = 0;
         startingPosition = transform.position;
         maxPosition = startingPosition + maxPositionDistance;
-        
+        rotationDirection = UnityEngine.Random.insideUnitSphere;
     }
 
 
@@ -101,6 +106,16 @@ public class ObstacleController : MonoBehaviour
     {
         UpdateMovement();
         UpdateTimer();
+        UpdateRotation();
+    }
+
+    private void UpdateRotation()
+    {
+        if (!rotationActive)
+        {
+            return;
+        }
+        transform.Rotate(rotationDirection * rotationSpeed * Time.deltaTime);
     }
 
     private void OnDestroy()
