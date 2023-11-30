44fac7a30f3182a7256f8e01099e5ce6cd9f3d8c
RicardoPenaP <ricardopena241197@gmail.com>
2023-11-30 18:46:52 -0300

    Updated main menu class
    Added the susbcription to the all buttons on click event and added each button public event to be subcribed

--- RocketLaunch/Assets/Scrips/Menus/MainMenu/MainMenu.cs
+++ RocketLaunch/Assets/Scrips/Menus/MainMenu/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class MainMenu : Menu<MainMenu>
 {
@@ -13,12 +14,93 @@ public class MainMenu : Menu<MainMenu>
     [SerializeField] private Button creditsButton;
     [SerializeField] private Button exitButton;
 
+    public event Action OnPlayButtonPressed;
+    public event Action OnInstructionsButtonPressed;
+    public event Action OnSettingsButtonPressed;
+    public event Action OnCreditsButtonPressed;
+
 
     protected override void Awake()
     {
         base.Awake();
 
-        exitButton.onClick.AddListener(ExitButton_OnClick);
+        if (playButton)
+        {
+            playButton.onClick.AddListener(PlayButton_OnClick);
+        }
+
+        if (instrucctionsButton)
+        {
+            instrucctionsButton.onClick.AddListener(InstructionsButton_OnClick);
+        }
+
+        if (settingsButton)
+        {
+            settingsButton.onClick.AddListener(SettingsButton_OnClick);
+        }
+
+        if (creditsButton)
+        {
+            creditsButton.onClick.AddListener(CreditsButton_OnClick);
+        }
+
+        if (exitButton)
+        {
+            exitButton.onClick.AddListener(ExitButton_OnClick);
+        }
+        
+    }
+
+    private void OnDestroy()
+    {
+        if (playButton)
+        {
+            playButton.onClick.RemoveListener(PlayButton_OnClick);
+        }
+
+        if (instrucctionsButton)
+        {
+            instrucctionsButton.onClick.RemoveListener(InstructionsButton_OnClick);
+        }
+
+        if (settingsButton)
+        {
+            settingsButton.onClick.RemoveListener(SettingsButton_OnClick);
+        }
+
+        if (creditsButton)
+        {
+            creditsButton.onClick.AddListener(CreditsButton_OnClick);
+        }
+
+        if (exitButton)
+        {
+            exitButton.onClick.RemoveListener(ExitButton_OnClick);
+        }
+    }
+
+    private void PlayButton_OnClick()
+    {
+        OnPlayButtonPressed?.Invoke();
+        CloseMenu();
+    }
+
+    private void InstructionsButton_OnClick()
+    {
+        OnInstructionsButtonPressed?.Invoke();
+        CloseMenu();
+    }
+
+    private void SettingsButton_OnClick()
+    {
+        OnSettingsButtonPressed?.Invoke();
+        CloseMenu();
+    }
+
+    private void CreditsButton_OnClick()
+    {
+        OnCreditsButtonPressed?.Invoke();
+        CloseMenu();
     }
 
     private void ExitButton_OnClick()
