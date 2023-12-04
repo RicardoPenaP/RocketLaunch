ffd52b9c56e2d5defedd4980caad80877a7b39cb
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-04 17:48:49 -0300

    Implemented feature
    Implemented camera changing methods in the Main Menu Camera class

--- RocketLaunch/Assets/Scrips/Cameras/MainMenuCameraMananger.cs
+++ RocketLaunch/Assets/Scrips/Cameras/MainMenuCameraMananger.cs
@@ -9,13 +9,31 @@ public class MainMenuCameraMananger : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera defaulVirtualCamera;
     [SerializeField] private CinemachineVirtualCamera upgradeRocketVirtualCamera;
 
+    private void Start()
+    {
+        if (UpgradeRocketMenu.Instance)
+        {
+            UpgradeRocketMenu.Instance.OnMenuOpened += UpgradeRocketMenu_OnMenuOpened;
+            UpgradeRocketMenu.Instance.OnGoBackButtonPressed += UpgradeRocketMenu_OnGoBackButtonPressed;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (UpgradeRocketMenu.Instance)
+        {
+            UpgradeRocketMenu.Instance.OnMenuOpened -= UpgradeRocketMenu_OnMenuOpened;
+            UpgradeRocketMenu.Instance.OnGoBackButtonPressed -= UpgradeRocketMenu_OnGoBackButtonPressed;
+        }
+    }
+
     private void UpgradeRocketMenu_OnMenuOpened()
     {
         defaulVirtualCamera.gameObject.SetActive(false);
         upgradeRocketVirtualCamera.gameObject.SetActive(true);
     }
 
-    private void UpgradeRocketMenu_OnMenuClosed()
+    private void UpgradeRocketMenu_OnGoBackButtonPressed()
     {
         upgradeRocketVirtualCamera.gameObject.SetActive(false);
         defaulVirtualCamera.gameObject.SetActive(true);        
