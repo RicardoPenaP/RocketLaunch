3a4c9913c6ba68a607aef571985bf801324a7255
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-05 18:13:28 -0300

    Created class
    Created Mission Mananger class

---
+++ RocketLaunch/Assets/Scrips/Manangers/MissionMananger/MissionMananger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionMananger : MonoBehaviour
+{
+    public MissionMananger Instance { get; private set; }
+
+
+
+}
