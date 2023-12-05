84ed978329558b2ffd60bc6a13f513d217b9a528
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-05 18:21:41 -0300

    Implemented feature
    Implemented Mission class method to get the class members

---
+++ RocketLaunch/Assets/Scrips/Manangers/MissionMananger/EstelarSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EstelarSystem
+{
+    [SerializeField] private Mission[] missions;
+}
--- RocketLaunch/Assets/Scrips/Manangers/MissionMananger/Mission.cs
+++ RocketLaunch/Assets/Scrips/Manangers/MissionMananger/Mission.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameSceneManagement;
 
 [System.Serializable]
 public class Mission 
 {
+    [SerializeField] private GameScene gameScene;
     [SerializeField] private int missionIndex;
     [SerializeField] private bool unlocked = false;
     [SerializeField] private bool completed = false;
+
+    
+    public GameScene GetGameScene()
+    {
+        return gameScene;
+    }
+
+    public int GetMissionIndex()
+    {
+        return missionIndex;
+    }
+
+    public bool GetUnlocked()
+    {
+        return unlocked;
+    }
+
+    public bool GetCompleted()
+    {
+        return completed;
+    }
 }
