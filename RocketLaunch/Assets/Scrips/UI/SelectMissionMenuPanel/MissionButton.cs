f02c13d4dade344b9d6bfacac74ab4b4432de421
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-05 21:00:19 -0300

    Implemented feature
    Implemented On Any Mission Button Pressed static event for the same name class

--- RocketLaunch/Assets/Scrips/UI/SelectMissionMenuPanel/MissionButton.cs
+++ RocketLaunch/Assets/Scrips/UI/SelectMissionMenuPanel/MissionButton.cs
@@ -3,16 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class MissionButton : MonoBehaviour
 {
+    public static event Action<StelarSystemID, int> OnAnyMissionButtonPressed;
     [Header("Mission Button")]
     [SerializeField] private Image lockedImage;
     [SerializeField] private Button button;
     [SerializeField] private TextMeshProUGUI buttonText;
 
+    private StelarSystemID stelarSystemID;
+    private int missionIndex;
+
+    private void Awake()
+    {
+        if (button)
+        {
+            button.onClick.AddListener(Button_OnClick);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (button)
+        {
+            button.onClick.RemoveListener(Button_OnClick);
+        }
+    }
+
+    private void Button_OnClick()
+    {
+        OnAnyMissionButtonPressed?.Invoke(stelarSystemID, missionIndex);
+    }
+
     public void SetMissionButtonIndex(StelarSystemID stelarSystemID, int missionIndex)
     {
+        this.stelarSystemID = stelarSystemID;
+        this.missionIndex = missionIndex;
         buttonText.text = $"Mission {(int)stelarSystemID}- {missionIndex}";
     }
 
