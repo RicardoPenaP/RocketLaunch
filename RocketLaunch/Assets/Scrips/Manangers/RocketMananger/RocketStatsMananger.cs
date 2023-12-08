d00891b60e49f4d96dd6717988d1a3cac24845ef
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-08 20:41:39 -0300

    Implemented feature
    Implemented Rocket Stat Mananger get methods

--- RocketLaunch/Assets/Scrips/Manangers/RocketMananger/RocketStatsMananger.cs
+++ RocketLaunch/Assets/Scrips/Manangers/RocketMananger/RocketStatsMananger.cs
@@ -11,6 +11,7 @@ public class RocketStatsMananger : MonoBehaviour
     public static RocketStatsMananger Instance { get; private set; }
 
     [Header("Rocket Stats Mananger")]
+    [SerializeField] private RocketStatsData rocketStatsData;
     [SerializeField] private int statsPointsGivenPerLevelUp = 6;
 
     public event Action<int> OnCurrentStatPointsChanged;   
@@ -93,10 +94,42 @@ public class RocketStatsMananger : MonoBehaviour
     public RocketStat GetRocketStat(StatType statType)
     {
         return rocketStats[(int)statType];
-    }
+    }   
 
     public int GetCurrentStatPoints()
     {
         return currentStatPoints;
     }
+
+    public float GetMainEngineSpeedMultiplierAugmentCoeficient() => rocketStatsData.GetMainEngineSpeedMultiplierAugmentCoeficient();
+
+    public float GetMainEngineTemperatureMultiplierAugmentCoeficient() => rocketStatsData.GetMainEngineTemperatureMultiplierAugmentCoeficient();
+
+    public float GetMainEngineFuelConsumptionMultiplierAugmentCoeficient() => rocketStatsData.GetMainEngineFuelConsumptionMultiplierAugmentCoeficient();
+
+    public float GetSideEngineTurningSpeedMultiplierAugmentCoeficient() => rocketStatsData.GetSideEngineTurningSpeedMultiplierAugmentCoeficient();
+
+    public float GetSideEngineTemperatureMultiplierAugmentCoeficient() => rocketStatsData.GetSideEngineTemperatureMultiplierAugmentCoeficient();
+
+    public float GetSideEngineFuelConsumptionMultiplierAugmentCoeficient() => rocketStatsData.GetSideEngineFuelConsumptionMultiplierAugmentCoeficient();
+
+    public float GetMaxTemperatureMultiplierAugmentCoeficient() => rocketStatsData.GetMaxTemperatureMultiplierAugmentCoeficient();
+
+    public float GetCoolingSpeedMultiplierAugmentCoeficient() => rocketStatsData.GetCoolingSpeedMultiplierAugmentCoeficient();
+
+    public float GetOverheatTimeMultiplierAugmentCoeficient() => rocketStatsData.GetOverheatTimeMultiplierAugmentCoeficient();
+
+    public float GetAngularDragMultiplierAugmentCoeficient() => rocketStatsData.GetAngularDragMultiplierAugmentCoeficient();
+
+    public float GetGreenAreaMultiplierAugmentCoeficient() => rocketStatsData.GetGreenAreaMultiplierAugmentCoeficient();
+
+    public float GetYellowAreaMultiplierAugmentCoeficient() => rocketStatsData.GetYellowAreaMultiplierAugmentCoeficient();
+
+    public float GetEffectivePercentageMultiplierAugmentCoeficient() => rocketStatsData.GetEffectivePercentageMultiplierAugmentCoeficient();
+
+    public float GetPullDistanceMultiplierAugmentCoeficient() => rocketStatsData.GetPullDistanceMultiplierAugmentCoeficient();
+
+    public float GetExperienceMultiplierAugmentCoeficient() => rocketStatsData.GetExperienceMultiplierAugmentCoeficient();
+
+    public float GetFuelCapacityMultiplierAugmentCoeficient() => rocketStatsData.GetFuelCapacityMultiplierAugmentCoeficient();
 }
