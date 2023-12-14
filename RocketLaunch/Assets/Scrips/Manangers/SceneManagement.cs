8bc6c3db2c67c21819c6cf6670f271cbdcb6b0ee
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-14 17:56:27 -0300

    Added new game scenes denomination

--- RocketLaunch/Assets/Scrips/Manangers/SceneManagement.cs
+++ RocketLaunch/Assets/Scrips/Manangers/SceneManagement.cs
@@ -4,7 +4,9 @@ using UnityEngine.SceneManagement;
 
 namespace GameSceneManagement 
 {
-    public enum GameScene { MainMenu,TestLevel, Level1, Level2, Level3, Level4, level5}
+    public enum GameScene { MainMenu,TestLevel, Level1_1, Level1_2, Level1_3, Level1_4, Level1_5,
+                            Level2_1, Level2_2, Level2_3, Level424, Level2_5
+    }
     public static class SceneManagement
     {
         private static GameScene currentGameScene = GameScene.MainMenu;
