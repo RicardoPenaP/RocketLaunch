d69f1b37cfaabb5e0a8335d211a11db72928e935
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-15 15:03:44 -0300

    Implemented feature
    Implemented Obstacle Shooter Shoot logic and method

--- RocketLaunch/Assets/Scrips/Obstacles/ObstacleShooter.cs
+++ RocketLaunch/Assets/Scrips/Obstacles/ObstacleShooter.cs
@@ -4,6 +4,67 @@ using UnityEngine;
 
 public class ObstacleShooter : MonoBehaviour
 {
-    //[Header("Obstacle Shooter")]
-    //[SerializeField]
+    [Header("Obstacle Shooter")]
+    [SerializeField] private ObstacleAmmo[] obstacleAmmoPrefabs;
+    [SerializeField] private Transform[] shootPositions;
+    [Tooltip("The amount of shoots per second")]
+    [SerializeField] private float shootRate = 1f;
+    [SerializeField] private bool shootFromAllShootPositionsAtTheSameTime = false;
+
+    private bool canShoot = false;
+    private float shootRestTime;
+    private int shootPositionIndex = 0;
+
+    private void Awake()
+    {
+        shootRestTime = 1 / shootRate;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ShootRestRoutine());
+    }
+
+    private void Update()
+    {
+        if (canShoot)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        canShoot = false;
+        //Shoot behaviour
+        if (shootFromAllShootPositionsAtTheSameTime)
+        {
+            foreach (Transform shootPosTranform in shootPositions)
+            {
+                int ammoIndex = Random.Range(0, obstacleAmmoPrefabs.Length);
+                Instantiate(obstacleAmmoPrefabs[ammoIndex], shootPosTranform.position, transform.rotation);
+            }
+        }
+        else
+        {
+            int ammoIndex = Random.Range(0, obstacleAmmoPrefabs.Length);
+            Instantiate(obstacleAmmoPrefabs[ammoIndex], shootPositions[shootPositionIndex].position, transform.rotation);
+            shootPositionIndex = shootPositionIndex < shootPositions.Length - 1 ? shootPositionIndex + 1 : 0;
+        }
+
+        StartCoroutine(ShootRestRoutine());
+    }
+
+    private IEnumerator ShootRestRoutine()
+    {
+        float timer = 0;
+
+        while (timer < shootRestTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        canShoot = true;
+    }
 }
