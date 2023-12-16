4bc4beff5ed1fe46e918131b2a5ea1d0e9d289f5
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-15 21:45:21 -0300

    Updated feature
    Updated instruction tex for each instruction button

--- RocketLaunch/Assets/Scrips/UI/InstructionsMenuPanel/InstructionsText.cs
+++ RocketLaunch/Assets/Scrips/UI/InstructionsMenuPanel/InstructionsText.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class InstructionsText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Instructions Text")]
+    [SerializeField] private TextMeshProUGUI instructionsText;
+
+    private void Start()
     {
-        
+        InstructionsButton.OnAnyInstructionsButtonPressed += InstructionsButton_OnAnyInstructionsButtonPressed;
+        if (InstructionsMenu.Instance)
+        {
+            InstructionsMenu.Instance.OnMenuOpened += InstructionsMenu_OnMenuOpened;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        InstructionsButton.OnAnyInstructionsButtonPressed -= InstructionsButton_OnAnyInstructionsButtonPressed;
+        if (InstructionsMenu.Instance)
+        {
+            InstructionsMenu.Instance.OnMenuOpened -= InstructionsMenu_OnMenuOpened;
+        }
+    }
+
+    private void InstructionsButton_OnAnyInstructionsButtonPressed(string instructionsText)
+    {
+        this.instructionsText.text = instructionsText;
+    }
+
+    private void InstructionsMenu_OnMenuOpened()
+    {
+        this.instructionsText.text = "";
     }
 }
