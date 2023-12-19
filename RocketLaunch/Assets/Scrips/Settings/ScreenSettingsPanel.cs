44a5090d793dbf15461db0e195c6cd7ab90b4996
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-19 15:58:04 -0300

    Renamed class

---
+++ RocketLaunch/Assets/Scrips/Settings/ScreenSettingsPanel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Settings;
+
+public class ScreenSettingsPanel : MonoBehaviour
+{
+    [Header("Screen Settings Controller")]
+    [SerializeField] private Dropdown qualityDropdown;
+    [SerializeField] private Dropdown maxFPSDropdown;
+    [SerializeField] private Button fullScreenButton;
+    [SerializeField] private Button fpsCounterButton;
+   
+}
