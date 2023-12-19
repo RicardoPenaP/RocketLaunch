1685ee9a50196decda5b1b8a4c7279a45b5ca2ba
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-19 14:56:25 -0300

    Implemented feature
    Implemented Settings Button class functionalities

--- RocketLaunch/Assets/Scrips/UI/SettingsMenuPanel/SettingsButton.cs
+++ RocketLaunch/Assets/Scrips/UI/SettingsMenuPanel/SettingsButton.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class SettingsButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action<SettingsPanel> OnAnySettingsButtonPressed;
+
+    [Header("Settings Button")]
+    [SerializeField] private Button button;
+    [SerializeField] private SettingsPanel settingsPanelPrefab;
+
+    private void Awake()
     {
-        
+        if (button)
+        {
+            button.onClick.AddListener(Button_OnClick);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (button)
+        {
+            button.onClick.RemoveListener(Button_OnClick);
+        }
     }
+
+    private void Button_OnClick()
+    {
+        OnAnySettingsButtonPressed?.Invoke(settingsPanelPrefab);
+    }
+
+
 }
