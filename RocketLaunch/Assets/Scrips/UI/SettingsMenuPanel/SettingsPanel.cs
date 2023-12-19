4500aeb63f97c97cd8f6ea3823bcac581dd4041f
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-19 14:48:33 -0300

    Renamed asset

--- RocketLaunch/Assets/Scrips/UI/SettingsMenuPanel/SettingsPanel.cs
+++ RocketLaunch/Assets/Scrips/UI/SettingsMenuPanel/SettingsPanel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsPanl : MonoBehaviour
+public class SettingsPanel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
