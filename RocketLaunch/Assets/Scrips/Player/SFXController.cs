c7082fbf552a18027165dafb4e3bc3a44283a484
RicardoPenaP <ricardopena241197@gmail.com>
2023-12-28 19:26:29 -0300

    Created class
    Created SFX Controller class

---
+++ RocketLaunch/Assets/Scrips/Player/SFXController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    [Header("SFX Controller")]
+    [SerializeField] private AudioSource primaryAudioSource;
+    [SerializeField] private AudioSource secondaryAudioSource;
+}
