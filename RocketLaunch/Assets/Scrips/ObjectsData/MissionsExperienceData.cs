54b4907d0a32f3125aebfeb63c1dd6e41ff367ae
RicardoPenaP <ricardopena241197@gmail.com>
2024-01-07 19:19:47 -0300

    Created class
    Created Missions Experience Data class

---
+++ RocketLaunch/Assets/Scrips/ObjectsData/MissionsExperienceData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewMissionExperienceData", menuName = "Mission Experience Data")]
+public class MissionsExperienceData : ScriptableObject
+{
+    [Header("Missions Experience Data")]
+    //[SerializeField] private Dictionary<> missionsExperienceDictionary;
+}
