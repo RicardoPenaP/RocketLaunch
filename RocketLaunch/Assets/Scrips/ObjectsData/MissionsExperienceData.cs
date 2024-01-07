3af6a67b973e9ad8dd0236d826432874447f678a
RicardoPenaP <ricardopena241197@gmail.com>
2024-01-07 19:39:55 -0300

    Updated feature
    Updated Mission Experience Data dictionary to a list

--- RocketLaunch/Assets/Scrips/ObjectsData/MissionsExperienceData.cs
+++ RocketLaunch/Assets/Scrips/ObjectsData/MissionsExperienceData.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GameSceneManagement;
+using System;
 
 
 [CreateAssetMenu(fileName = "NewMissionExperienceData", menuName = "Mission Experience Data")]
 public class MissionsExperienceData : ScriptableObject
 {
+    [Serializable]
+    public class MissionExperience
+    {
+        public GameScene gameScene;
+        public float experience;
+    }
+
     [Header("Missions Experience Data")]
-    [SerializeField] private Dictionary<GameScene,float> missionsExperienceDictionary;
+    [SerializeField] private List<MissionExperience> missionsExperienceList;
 
     public float GetMissionExperience(GameScene gameScene)
-    {
-        if (missionsExperienceDictionary.ContainsKey(gameScene))
+    {        
+        foreach (MissionExperience missionExperience in missionsExperienceList)
         {
-            return missionsExperienceDictionary[gameScene];
+            if (missionExperience.gameScene == gameScene)
+            {
+                return missionExperience.experience;
+            }
         }
 
         return 0f;
