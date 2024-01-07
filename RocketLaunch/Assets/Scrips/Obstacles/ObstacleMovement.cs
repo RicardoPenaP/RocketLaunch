33602635f11d996d07b7f2f3109b7d5cd8e3b10a
RicardoPenaP <ricardopena241197@gmail.com>
2024-01-07 18:01:35 -0300

    Implemented feature
    Implemented Obstacle Movement class functionalities

--- RocketLaunch/Assets/Scrips/Obstacles/ObstacleMovement.cs
+++ RocketLaunch/Assets/Scrips/Obstacles/ObstacleMovement.cs
@@ -8,12 +8,18 @@ public class ObstacleMovement : MonoBehaviour
     [SerializeField] private bool canMove = false;
     [SerializeField] private bool showGizmos = false;
     [SerializeField] private Vector3 movementDistance;
-    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField,Min(0f)] private float movementSpeed = 10f;
 
 
     private Vector3 startPosition;
     private Vector3 maxPosition;
     private Vector3 minPosition;
+    private Vector3 movementDirection;
+
+    private float maxDistance;
+    //private float changeDirectionCooldownTime = 0.1f;
+
+    //private bool canChangeDirection = true;
 
     private void OnDrawGizmosSelected()
     {
@@ -22,33 +28,100 @@ public class ObstacleMovement : MonoBehaviour
             return;
         }
 
-        Vector3 startPoint = transform.position;
-        Vector3 finishPoint = transform.position;
+        Vector3 position;
+
+        if (Application.isPlaying)
+        {
+            position = startPosition;
+        }
+        else
+        {
+            position = transform.position;
+        }
+
+        Vector3 startPoint = position;
+        Vector3 finishPoint = position;
         startPoint.y -= movementDistance.y / 2;
         finishPoint.y += movementDistance.y / 2;
 
         Gizmos.color = Color.green;
         Gizmos.DrawLine(startPoint, finishPoint);
 
-        startPoint = transform.position;
+        startPoint = position;
         startPoint.x -= movementDistance.x / 2;
-        finishPoint = transform.position;
+        finishPoint = position;
         finishPoint.x += movementDistance.x / 2;
 
         Gizmos.color = Color.red;
         Gizmos.DrawLine(startPoint, finishPoint);
 
-        startPoint = transform.position;
+        startPoint = position;
         startPoint.z -= movementDistance.z / 2;
-        finishPoint = transform.position;
+        finishPoint = position;
         finishPoint.z += movementDistance.z / 2;
 
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(startPoint, finishPoint);
     }
 
+    private void OnValidate()
+    {
+        if (movementDistance.x < 0)
+        {
+            movementDistance.x = 0;
+        }
+
+        if (movementDistance.y < 0)
+        {
+            movementDistance.y = 0;
+        }
+
+        if (movementDistance.z < 0)
+        {
+            movementDistance.z = 0;
+        }
+    }
+
     private void Awake()
     {
         startPosition = transform.position;
+        maxPosition = startPosition + (movementDistance / 2);
+        minPosition = startPosition - (movementDistance / 2);       
+
+        movementDirection = (maxPosition - transform.position).normalized;
+
+        maxDistance = Vector3.Distance(startPosition, maxPosition);
     }
+
+    private void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        transform.position += movementDirection * movementSpeed * Time.fixedDeltaTime;
+
+        if (Vector3.Distance(transform.position, startPosition) >= maxDistance)
+        {
+            if (movementDirection == (maxPosition - transform.position).normalized)
+            {
+                movementDirection = (minPosition - transform.position).normalized;
+            }
+            else
+            {
+                movementDirection = (maxPosition - transform.position).normalized;
+            }
+            //canChangeDirection = false;
+            //StartCoroutine(ChangeDirectionCooldownRoutine());
+        }
+
+    }
+
+    //private IEnumerator ChangeDirectionCooldownRoutine()
+    //{
+    //    yield return new WaitForSeconds(changeDirectionCooldownTime);
+    //    canChangeDirection = true; 
+    //}
+
 }
