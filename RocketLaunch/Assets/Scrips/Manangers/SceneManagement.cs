64822e80b3a2fbad4e4f8fcb5a4fc1575708bde1
RicardoPenaP <ricardopena241197@gmail.com>
2024-01-10 18:59:13 -0300

    Added scenes to the in build scenes

--- RocketLaunch/Assets/Scrips/Manangers/SceneManagement.cs
+++ RocketLaunch/Assets/Scrips/Manangers/SceneManagement.cs
@@ -10,6 +10,7 @@ namespace GameSceneManagement
         Level1_1, Level1_2, Level1_3, Level1_4, Level1_5,
         Level1_6, Level1_7, Level1_8, Level1_9, Level1_10,
         Level2_1, Level2_2, Level2_3, Level2_4, Level2_5,
+        Level2_6, Level2_7, Level2_8, Level2_9, Level2_10,
         Level3_1, Level3_2, Level3_3, Level3_4, Level3_5 
     }
     public static class SceneManagement
